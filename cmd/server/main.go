27295c98a57d39f59a49dc9a3e2aa88aea0d9321
Darwin D Wu <darwin67@users.noreply.github.com>
2023-12-13 15:00:53 -0800

    change default to health check

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -16,10 +17,22 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Proxy Server!!"))
-	})
+	r.Get("/", healthCheck)
 
 	fmt.Printf("Listening on port %d\n", port)
 	http.ListenAndServe(fmt.Sprintf(":%d", port), r)
+}
+
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	resp := map[string]string{"result": "ok"}
+
+	byt, err := json.Marshal(resp)
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte("{ \"error\": \"failed\" }"))
+		return
+	}
+
+	w.Write(byt)
 }
