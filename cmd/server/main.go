0500fdd049d208f217ea64158876a38a16625975
Darwin D Wu <darwin67@users.noreply.github.com>
2023-12-13 14:57:34 -0800

    make it a proper server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+const (
+	port = 9999
 )
 
 func main() {
-	fmt.Println("Proxy Server!!")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Proxy Server!!"))
+	})
+
+	fmt.Printf("Listening on port %d\n", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), r)
 }
