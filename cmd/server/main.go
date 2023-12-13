b40f97cf0935a8af7643c12026831f23cdd73d78
Darwin D Wu <darwin67@users.noreply.github.com>
2023-12-13 15:42:08 -0800

    make server to utilize http2

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -18,9 +21,18 @@ func main() {
 	r.Use(middleware.Logger)
 
 	r.Get("/", healthCheck)
+	r.Post("/connect", longlive)
 
 	fmt.Printf("Listening on port %d\n", port)
-	http.ListenAndServe(fmt.Sprintf(":%d", port), r)
+
+	h2s := &http2.Server{}
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: h2c.NewHandler(r, h2s),
+	}
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatalf("error running h2 server: %v", err)
+	}
 }
 
 func healthCheck(w http.ResponseWriter, r *http.Request) {
@@ -35,4 +47,30 @@ func healthCheck(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Write(byt)
+}
+
+func longlive(w http.ResponseWriter, r *http.Request) {
+	hj, ok := w.(http.Hijacker)
+	if !ok {
+		http.Error(w, "server doesn't support hijacking", http.StatusInternalServerError)
+		return
+	}
+
+	conn, bufrw, err := hj.Hijack()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer conn.Close()
+
+	bufrw.WriteString("Now we're speaking raw TCP. Say hi: ")
+	bufrw.Flush()
+	// s, err := bufrw.ReadString('\n')
+	// if err != nil {
+	// 	log.Printf("error reading string: %v", err)
+	// 	return
+	// }
+
+	// fmt.Fprintf(bufrw, "You said: %q\nBye.\n", s)
+	// bufrw.Flush()
 }
