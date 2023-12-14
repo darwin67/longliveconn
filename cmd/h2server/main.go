3e295bffcc91450cc7dfca06bba126363777494b
Darwin D Wu <darwin67@users.noreply.github.com>
2023-12-14 14:48:27 -0800

    add tcp listener as well

--- cmd/h2server/main.go
+++ cmd/h2server/main.go
@@ -3,35 +3,87 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"golang.org/x/net/http2"
-	"golang.org/x/net/http2/h2c"
 	"log"
+	"net"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 )
 
 const (
-	port = 9999
+	tcpPort  = 9999
+	httpPort = 9990
 )
 
 func main() {
+	addr := fmt.Sprintf(":%d", tcpPort)
+
+	// TCP listener
+	go func() {
+		listener, err := net.Listen("tcp", addr)
+		if err != nil {
+			fmt.Println("error listening to TCP: ", err)
+			return
+		}
+		defer listener.Close()
+		fmt.Println("TCP listener on: ", listener.Addr())
+
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				fmt.Println("error accepting conn: ", err)
+				continue
+			}
+
+			go handleConnection(conn)
+		}
+	}()
+
+	// APIs
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
 	r.Get("/", healthCheck)
 	r.Post("/connect", longlive)
 
-	fmt.Printf("Listening on port %d\n", port)
+	fmt.Printf("HTTP server listening on port %d\n", httpPort)
 
 	h2s := &http2.Server{}
 	srv := &http.Server{
-		Addr:    fmt.Sprintf(":%d", port),
+		Addr:    fmt.Sprintf(":%d", httpPort),
 		Handler: h2c.NewHandler(r, h2s),
 	}
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatalf("error running h2 server: %v", err)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	fmt.Println("Accepted conn from: ", conn.RemoteAddr())
+
+	// We can read and write to the connection using conn.Read and conn.Write
+	// For example, echoing the received data back to the client:
+	buffer := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Println("error reading from conn: ", err)
+			return
+		}
+
+		data := buffer[:n]
+		fmt.Printf("received data: %s\n", data)
+
+		_, err = conn.Write(data)
+		if err != nil {
+			fmt.Println("error writing to conn: ", err)
+			return
+		}
 	}
 }
 
