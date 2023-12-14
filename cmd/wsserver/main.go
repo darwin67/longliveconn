c20a3b1a8defe01e8d0389d8e94d0b4d51445b15
Darwin D Wu <darwin67@users.noreply.github.com>
2023-12-14 12:07:12 -0800

    websocket sample

--- cmd/wsserver/main.go
+++ cmd/wsserver/main.go
@@ -2,8 +2,46 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var (
+	port     = 9990
+	upgrader = websocket.Upgrader{}
 )
 
 func main() {
-	fmt.Printf("Websocket Server")
+	fmt.Println("Websocket Server!!!")
+
+	http.HandleFunc("/echo", echo)
+
+	addr := fmt.Sprintf(":%d", port)
+	fmt.Printf("Listening on %s\n", addr)
+
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade: ", err)
+		return
+	}
+	defer c.Close()
+
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Print("read: ", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		if err := c.WriteMessage(mt, message); err != nil {
+			log.Println("write: ", err)
+			break
+		}
+	}
 }
